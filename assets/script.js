fd572fafdd9e381bebb92f9253eebb49e4b2981d
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-02-12 13:41:26 -0500

    search button now returns weather in object

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
-var getCityWeather = function() {
-fetch('api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=f0779280ef21eceac2a12efca6febb5')
-}
+var apiWeatherKey = '9f0779280ef21eceac2a12efca6febb5'
+var queryUrl = 'HTTPS://api.openweathermap.org/data/2.5/weather?q='+ userInput + '&appid=' + apiWeatherKey;
+var searchBtn = document.getElementById('searchBtn')
+
+
+
+searchBtn.addEventListener('click', function(getCityWeather){
+getCityWeather.preventDefault();
+    var userInput = document.getElementById('userInput').value  
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ userInput + '&appid=' + apiWeatherKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+})
