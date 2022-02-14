f70a9c78a50965f39ec8b3371b25b2876c96c029
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-02-13 23:46:55 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # weather-dashboard
+
+# plan-the-day
+
+## Purpose
+A website that shows users weather in a certain area
+
+## Built With
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+* Ajax
+
+## Website
+[View Webpage](https://xcamaj.github.io/weather-dashboard/)
+![Webpage Screenshot](./assets/images/screenshot.png)
+
+## Contribution
+Made by Xhemajl Camaj
--- assets/script.js
+++ assets/script.js
@@ -7,11 +7,31 @@ var searchBtn = document.getElementById('searchBtn')
 searchBtn.addEventListener('click', function(getCityWeather){
 getCityWeather.preventDefault();
     var userInput = document.getElementById('userInput').value  
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ userInput + '&appid=' + apiWeatherKey)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiWeatherKey)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
     })
-})
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + userInput + '&appid=' +apiWeatherKey)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+    })  
+
+var lat = response.coord.lat;
+var lon = response.coord.lon;
+var queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + apiWeatherKey + "&lat=" + lat + "&lon=" + lon;
+
+$.ajax({
+    url: queryURLUV,
+    method: 'GET'
+}).then(function (response) {
+    $('#uvl-display').empty();
+  
+});
+   
