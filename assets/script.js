4cebc804badcadeb78996854afbe180fe7ffe399
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-02-15 12:03:55 -0500

    temp wind humidity and uvi is now displayed for every city

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,53 @@
 var apiWeatherKey = '9f0779280ef21eceac2a12efca6febb5'
-var queryUrl = 'HTTPS://api.openweathermap.org/data/2.5/weather?q='+ userInput + '&appid=' + apiWeatherKey;
+var queryUrl = 'HTTPS://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiWeatherKey;
 var searchBtn = document.getElementById('searchBtn')
+var uvIndex = document.getElementById('uvi')
+var res;
+var cityName = document.getElementById('city-name');
+var currentTemp = document.getElementById('temp');
+var currentWind = document.getElementById('wind');
+var currentHumidity = document.getElementById('humidity');
 
 
-
-searchBtn.addEventListener('click', function(getCityWeather){
-getCityWeather.preventDefault();
-    var userInput = document.getElementById('userInput').value  
+searchBtn.addEventListener('click', function (getCityWeather) {
+    getCityWeather.preventDefault();
+    var userInput = document.getElementById('userInput').value
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiWeatherKey)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + userInput + '&appid=' +apiWeatherKey)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            res = data
+            var temp = parseFloat(res.main.temp)
+            $('#temp').empty()
+            $("#temp").text('Temp: ' + ((temp - 273.15) * 1.8) + 32);
+            $('#humidity').empty()
+            $('#humidity').text('Humidity: ' + res.main.humidity)
+            $('#wind').empty()
+            $('#wind').text('Wind: ' + res.wind.gust)
+            if (res && res.coord) {
+                var lat = res.coord.lat;
+                var lon = res.coord.lon;
+                var queryUrlUvi = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + apiWeatherKey + "&lat=" + lat + "&lon=" + lon;
+                $.ajax({
+                    url: queryUrlUvi,
+                    method: 'GET'
+                }).then(function (response) {
+                    console.log(response, "response")
+                    $('#uvi').empty();
+                    $('#uvi').text("UV Index: " + response.value)
+
+                });
+            }
+        })
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + userInput + '&appid=' + apiWeatherKey)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
         })
-    })  
+})
 
-var lat = response.coord.lat;
-var lon = response.coord.lon;
-var queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + apiWeatherKey + "&lat=" + lat + "&lon=" + lon;
 
-$.ajax({
-    url: queryURLUV,
-    method: 'GET'
-}).then(function (response) {
-    $('#uvl-display').empty();
-  
-});
-   
